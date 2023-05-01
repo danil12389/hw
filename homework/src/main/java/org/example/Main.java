66ede31e7a17df6b7eae68a15e4ef7486e6e7c3d
danil12389 <akverrr@gmail.com>
2023-05-01 13:11:27 +0400

    примеры функцю

--- homework/src/main/java/org/example/Main.java
+++ homework/src/main/java/org/example/Main.java
@@ -10,6 +10,8 @@ public class Main {
        //Пример кастомного функционального интерфейса, проверяет что в строке
         // первая буква - заглавная, остальные - строчные.
         CapitalLetterChecker<String> f = x -> x.substring(0, 1).equals(x.substring(0,1).toUpperCase() )&& x.substring(1).equals(x.substring(1).toLowerCase());
-        System.out.println(f.test("AAc"));
+        System.out.println(f.test("AAc")); //false
+        System.out.println(f.test("Abc")); //true
+        System.out.println(f.test("abc")); //false
     }
 }
