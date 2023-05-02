db0ad71c1bda18cc489cc99736ca53616ab37f4b
danil12389 <akverrr@gmail.com>
2023-05-02 14:22:24 +0400

    как сделать то

--- homework/src/main/java/org/example/Main.java
+++ homework/src/main/java/org/example/Main.java
@@ -13,5 +13,18 @@ public class Main {
         System.out.println(f.test("AAc")); //false
         System.out.println(f.test("Abc")); //true
         System.out.println(f.test("abc")); //false
+
+        List<String> posts = Arrays.asList(
+                "Hi everyone #me #hello #me",
+                "Hi everyone. I am Alex #alex #helloworld #me #hello",
+                "Hi! I am Alex #alex #helloworld #me #hello"
+        );
+        List<String> result = new ArrayList<>();
+        posts.stream()
+                        .map(i -> i.split(""))
+        .filter(i -> Arrays.stream(i).allMatch(x -> x.substring(0).equals("#")))
+                .map(i -> result.add(i))
+         .forEach(System.out::println);
+        System.out.println(posts.size());
     }
 }
